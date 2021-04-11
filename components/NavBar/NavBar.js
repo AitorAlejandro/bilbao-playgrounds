8d4d8d2e16b5314e8b4e4c983d146caa6d7e7502
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-04-11 21:03:17 +0200

    NavBar component improvement

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -2,12 +2,42 @@ import Link from "next/link";
 
 export function NavBar() {
   return (
-    <nav>
-      <Link href="/">
-        <a>
-          <h1>Bilbao Playground</h1>
-        </a>
-      </Link>
+    <nav className="px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
+      <div className="flex flex-col items-center justify-center pt-20 text-center">
+        <Link href="/">
+          <a className="font-bold text-3xl text-gray-900">
+            <h1>Bilbao Playgrounds</h1>
+          </a>
+        </Link>
+        <p className="font-base text-base text-gray-600">
+          A collections of every playground in Bilbao to go with kids.
+        </p>
+      </div>
+      <div className="pt-6">
+        <div className="bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
+          <button className="outline-none focus:outline-none">
+            <svg
+              className="w-5 text-gray-600 h-5 cursor-pointer"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+            </svg>
+          </button>
+          <input
+            type="search"
+            name=""
+            id=""
+            placeholder="search for playgrounds"
+            x-model="q"
+            className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent"
+          />
+        </div>
+      </div>
     </nav>
   );
 }
