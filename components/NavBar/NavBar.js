4af1caabd43af8701a57047d0b6f3c83b284fc4b
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-04-11 22:37:23 +0200

    NavBar component - div with aria hidden instead of button

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -15,7 +15,7 @@ export function NavBar() {
       </div>
       <div className="pt-6">
         <div className="bg-white rounded flex items-center w-full p-3 shadow-sm border border-gray-200">
-          <button className="outline-none focus:outline-none">
+          <div className="outline-none focus:outline-none" aria-hidden="true">
             <svg
               className="w-5 text-gray-600 h-5 cursor-pointer"
               fill="none"
@@ -27,7 +27,7 @@ export function NavBar() {
             >
               <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
             </svg>
-          </button>
+          </div>
           <input
             type="search"
             name=""
