ab8fce861cab4fdb4d84d70618b17868410f5d99
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-04-13 00:11:28 +0200

    fix dynamic href

--- components/Playground/Playground.js
+++ components/Playground/Playground.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Playground({ slug, title }) {
   return (
-    <Link href={`/${slug}`}>
+    <Link href={`/playgrounds/${slug}`}>
       <a className="my-2 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
         <article className="overflow-hidden rounded-lg shadow-lg">
           <img
